4d14540c7bf22bf7fd28610a8f90463636f5d7e7
zhaitao <370985116@qq.com>
2018-04-24 13:20:57 +0800

    mix rn

--- app/src/main/java/mix_rn/zhai/con/mix_rn/MainActivity.kt
+++ app/src/main/java/mix_rn/zhai/con/mix_rn/MainActivity.kt
@@ -1,12 +1,16 @@
 package mix_rn.zhai.con.mix_rn
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.fab
+import kotlinx.android.synthetic.main.activity_main.toolbar
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +18,27 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(toolbar)
-
     fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show()
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (!Settings.canDrawOverlays(this)) {
+          val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+              Uri.parse("package:$packageName"))
+          startActivityForResult(intent, 0x01)
+        } else {
+          val intent = Intent(this, RNActivity::class.java)
+          startActivity(intent)
+        }
+      }
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    if (requestCode == 0x01) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (!Settings.canDrawOverlays(this)) {
+          // SYSTEM_ALERT_WINDOW permission not granted...
+        }
+      }
     }
   }
 
---
+++ app/src/main/java/mix_rn/zhai/con/mix_rn/RNActivity.kt
@@ -0,0 +1,77 @@
+package mix_rn.zhai.con.mix_rn
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.react.shell.MainReactPackage
+
+
+class RNActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
+  private var mReactRootView: ReactRootView? = null
+  private var mReactInstanceManager: ReactInstanceManager? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    mReactRootView = ReactRootView(this)
+    mReactInstanceManager = ReactInstanceManager.builder()
+        .setApplication(application)
+        .setBundleAssetName("index.android.bundle")
+        .setJSMainModulePath("index")
+        .addPackage(MainReactPackage())
+        .setUseDeveloperSupport(BuildConfig.DEBUG)
+        .setInitialLifecycleState(LifecycleState.RESUMED)
+        .build()
+
+    setContentView(mReactRootView)
+    mReactRootView?.startReactApplication(mReactInstanceManager, "APP", null)
+
+  }
+
+  override fun onBackPressed() {
+    if (mReactInstanceManager != null) {
+      mReactInstanceManager?.onBackPressed()
+    } else {
+      super.onBackPressed()
+    }
+  }
+
+  override fun invokeDefaultOnBackPressed() {
+    super.onBackPressed()
+  }
+
+  override fun onPause() {
+    super.onPause()
+
+    if (mReactInstanceManager != null) {
+      mReactInstanceManager!!.onHostPause(this)
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+
+    if (mReactInstanceManager != null) {
+      mReactInstanceManager!!.onHostResume(this, this)
+    }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+
+    if (mReactInstanceManager != null) {
+      mReactInstanceManager!!.onHostDestroy(this)
+    }
+  }
+
+  override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+    if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+      mReactInstanceManager?.showDevOptionsDialog()
+      return true
+    }
+    return super.onKeyUp(keyCode, event)
+  }
+}
