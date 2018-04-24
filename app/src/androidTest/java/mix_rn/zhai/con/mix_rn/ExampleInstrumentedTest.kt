795df4725d2c4df88a1b1daac0bbda0303f259cd
zhaitao <370985116@qq.com>
2018-04-24 12:06:11 +0800

    first

---
+++ app/src/androidTest/java/mix_rn/zhai/con/mix_rn/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package mix_rn.zhai.con.mix_rn
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getTargetContext()
+    assertEquals("mix_rn.zhai.con.mix_rn", appContext.packageName)
+  }
+}
